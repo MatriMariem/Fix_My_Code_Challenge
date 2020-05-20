cf7147848d668d6c4a59cb0f6d0b99fdc236388a
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-05-20 15:47:39 +0100

    add dpcstrings

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+""" square class """
 
 
 class square():
-
+    """ square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ instantiation """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,9 +17,11 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ perimeter """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ printable representation """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
