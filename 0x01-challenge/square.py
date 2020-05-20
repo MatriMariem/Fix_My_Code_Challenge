246dbe25c947e84158dac926ae6c5fc5a1fa3a79
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-05-20 15:42:17 +0100

    fix pep8

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
+
 class square():
-    
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
