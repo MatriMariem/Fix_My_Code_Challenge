ab099eb494f78a843f7539dc0cec241e8a4d64e8
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-05-21 23:35:03 +0100

    ffix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-""" square class """
+""" module """
 
 
-class square():
-    """ square class """
+class Square():
+    
+    width = 0
+    height = 0
 
+    
     def __init__(self, *args, **kwargs):
         """ instantiation """
         for key, value in kwargs.items():
@@ -14,17 +17,15 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """ perimeter """
+    def permiter_of_my_square(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ printable representation """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
